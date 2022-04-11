a3eb7d97cdd9783648e1764d753c36db7a812cd5
Lorenzo Capaldo <lorecapaldo@gmail.com>
2022-04-11 19:54:27 +0200

    lelle

--- classification/main.py
+++ classification/main.py
@@ -1,54 +1,37 @@
 import matplotlib as plt
 import mpl_toolkits
 import os
-from skimage.io import imread
-from skimage.transform import resize
+import cv2
 
-def resize_all(src, pklname, include, width=150, height=None):
-    """
-    load images from path, resize them and write them as arrays to a dictionary, 
-    together with labels and metadata. The dictionary is written to a pickle file 
-    named '{pklname}_{width}x{height}px.pkl'.
-     
-    Parameter
-    ---------
-    src: str
-        path to data
-    pklname: str
-        path to output file
-    width: int
-        target width of the image in pixels
-    include: set[str]
-        set containing str
-    """
-     
-    height = height if height is not None else width
-     
-    data = dict()
-    data['description'] = 'resized ({0}x{1})animal images in rgb'.format(int(width), int(height))
-    data['label'] = []
-    data['filename'] = []
-    data['data'] = []   
-     
-    pklname = f"{pklname}_{width}x{height}px.pkl"
- 
-    # read all images in PATH, resize and write to DESTINATION_PATH
-    for subdir in os.listdir(src):
-        if subdir in include:
-            print(subdir)
-            current_path = os.path.join(src, subdir)
- 
-            for file in os.listdir(current_path):
-                if file[-3:] in {'jpg', 'png'}:
-                    im = imread(os.path.join(current_path, file))
-                    im = resize(im, (width, height)) #[:,:,::-1]
-                    data['label'].append(subdir[:-4])
-                    data['filename'].append(file)
-                    data['data'].append(im)
- 
-        joblib.dump(data, pklname)
- lll
+def res(ratio):
 
+    list = []
+    
+    for i in range(1, 201):
+        print("foto pic ", i)
+        im = cv2.imread("C:/Users/lorec/friendly-pancake/classification/pic/books/pic{}.jpg".format(i),cv2.IMREAD_UNCHANGED)
+
+        scale = ratio # percent of original size
+        width = int(im.shape[1] * scale)
+        height = int(im.shape[0] * scale)
+        dim = (width, height)
+
+        im_resized = cv2.resize(im,dim,interpolation = cv2.INTER_AREA)
+        
+        filename = "C:/Users/lorec/friendly-pancake/classification/pic/books/pic_res{}.jpg".format(i)
+        cv2.imwrite(filename, im_resized)
+
+        list.append(im_resized)
+
+    return list
+
+
+def main():
+    ratio = 0.20
+
+    pics = res(ratio)
+
+    print("lista size {}".format(len(pics)))
 
 
 if __name__=="__main__":
