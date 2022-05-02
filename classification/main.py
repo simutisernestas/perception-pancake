d5e1daa96d157d2aa7a3d895654d998da24a855b
javAlborz <alborz.sa@gmail.com>
2022-05-02 13:39:55 +0200

    added SVM

--- classification/main.py
+++ classification/main.py
@@ -11,11 +11,21 @@ from sklearn.svm import LinearSVC
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-
 import pandas as pd
-
 from skimage.transform import resize
 from skimage.io import imread
+
+import timeit
+import time
+import datetime as dt
+from sklearn.decomposition import PCA
+from sklearn import datasets, svm, metrics
+from sklearn.datasets import fetch_openml
+import sklearn as sk
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import ssl
 
 # Helper function for plotting the fit of your SVM.
 def plot_fit(X, y, clf):
@@ -55,20 +65,40 @@ def main():
         print(f'loading... category : {i}')
         path=os.path.join(datadir,i).replace("\\","/")
         for img in os.listdir(path):
-            img_array=imread(os.path.join(path,img)) #replace doesn't seem to work. Path is messed up .replace("\\","/")
+            img_array=imread(os.path.join(path,img))
             img_resized=resize(img_array,(150,150,3))
             flat_data_arr.append(img_resized.flatten())
             target_arr.append(Categories.index(i))
         print(f'loaded category:{i} successfully')
     flat_data=np.array(flat_data_arr)
-    target=np.array(target_arr)
-    df=pd.DataFrame(flat_data) #dataframe
-    df['Target']=target
-    x=df.iloc[:,:-1] #input data 
-    y=df.iloc[:,-1] #output data
+    targets=np.array(target_arr)
+
+##  Can convert to Pandas
+    # df=pd.DataFrame(flat_data) #dataframe
+    # df['Target']=target
+    # x=df.iloc[:,:-1] #input data 
+    # y=df.iloc[:,-1] #output data
+    # print(np.shape(x))
+    # print(np.shape(y))
+
+
+    start = timeit.default_timer()
+
+    X_scaled = StandardScaler().fit_transform(flat_data)
+    pca = PCA(n_components = 2)
+    principalComponents = pca.fit_transform(X_scaled)
+    print(f'Principal components:{principalComponents[0]}')
+    X_train, X_test, y_train, y_test= train_test_split(principalComponents, targets, test_size=0.4)
+    clf = sk.svm.LinearSVC(penalty='l2', loss='squared_hinge', random_state=0, max_iter=10e4)
+    clf.fit(X_train, y_train)
+    clf.score(X_train, y_train)
+    clf.score(X_test, y_test)
+    pred = clf.predict(X_test)
     
-    print(x.shape())
-    print(y.shape())
+    stop = timeit.default_timer()    
+    print(f'accuracy score:{accuracy_score(y_test, pred)}') 
+    #Showing prediction
+    print('Time: ', stop - start)
     
 if __name__ == "__main__":
     main()
